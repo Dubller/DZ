fbb16b2979e8acda3a03dc42dc85158d13a19e3d
Dubller <dubller007@gmail.com>
2023-02-06 19:23:18 +0300

    Рomework

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,17 @@ public class Main {
         System.out.print("Hello world!");
         System.out.print(" Welcome to Java");
 
-        System.out.print(" 2");
     }
+
+    static void getmassage()
+    {
+        System.out.println("This is mothod");
+    }
+
+    static void finalImprovements()
+    {
+
+    }
+
+
 }
