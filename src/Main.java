d9efe6b55077e393654ece1bc05334c6637f4b8a
Dubller <dubller007@gmail.com>
2023-02-07 19:25:10 +0300

    Рomework

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,10 @@
-public class Main {
-    public static void main(String[] args) {
+public class Main
+{
+
+    public static void main(String[] args)
+    {
         System.out.print("Hello world!");
         System.out.print(" Welcome to Java");
-
     }
-
-    static void getMessage()
-    {
-        System.out.println("This is mothod");
-    }
-
-    static void finalImprovements()
-    {
-        System.out.println("123");
-
-    }
-
 
 }
