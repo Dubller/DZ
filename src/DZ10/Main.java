fc3210ce8596f3c08d85f764947e748a8dd60755
Dubller <dubller007@gmail.com>
2023-03-14 00:47:50 +0300

    Homework 10

--- src/DZ10/Main.java
+++ src/DZ10/Main.java
@@ -3,7 +3,7 @@ package DZ10;
 public class Main extends functional{
 
     public static void main(String[] args) {
-        String document = "1234-aSd-4321-ZXc-1q2E3e";
+        String document = "5254-aBa-4321-ZXa-1A2b";
 
 // Вывести на экран в одну строку два первых блока по 4 цифры.
         functional.printTwoBlock(document);
@@ -17,6 +17,28 @@ public class Main extends functional{
 //Вывести на экран только одни буквы из номера документа в
 //формате yyy/yyy/y/y в нижнем регистре.
         functional.deleteLetter(document);
+        System.out.println();
+
+//Вывести на экран буквы из номера документа в формате
+//"Letters:yyy/yyy/y/y" в верхнем регистре(реализовать с помощью
+//класса StringBuilder).
+        functional.uppercaseLetters(document);
+        System.out.println();
+
+//Проверить содержит ли номер документа последовательность abc и
+//вывети сообщение содержит или нет(причем, abc и ABC считается
+//одинаковой последовательностью).
+        functional.checkingLetters(document);
+        System.out.println();
+
+//Проверить начинается ли номер документа с последовательности
+//555.
+        functional.checkingNumbers(document);
+        System.out.println();
+
+// Проверить заканчивается ли номер документа на
+//последовательность 1a2b.
+        functional.checkingNumberEnds(document);
         System.out.println();
 
 
--- src/DZ10/functional.java
+++ src/DZ10/functional.java
@@ -13,9 +13,40 @@ public class functional {
     }
 
     public static void deleteLetter(String document) {
-       String[] doc = document.split("-");
-       String lettering = doc[1]+ "/" + doc[3] + "/" + doc[4].charAt(1) + "/" + doc[4].charAt(3);
-       System.out.println(lettering.toLowerCase());
+        String[] doc = document.split("-");
+        String lettering = doc[1] + "/" + doc[3] + "/" + doc[4].charAt(1) + "/" + doc[4].charAt(3);
+        System.out.println(lettering.toLowerCase());
+    }
+
+    public static void uppercaseLetters(String document) {
+        String[] doc = document.split("-");
+        StringBuilder i = new StringBuilder("Letters:");
+        i.append(doc[1].toUpperCase());
+        i.append("/");
+        i.append(doc[3].toUpperCase());
+        i.append("/");
+        i.append(doc[4].toUpperCase().charAt(1));
+        i.append("/");
+        i.append(doc[4].toUpperCase().charAt(3));
+        i.append("/");
+        System.out.println(i);
+    }
+
+    public static void checkingLetters(String documents) {
+        Boolean letter  = documents.toLowerCase().replaceAll("-", "").
+                replaceAll("\\d", "").contains("abc");
+        System.out.println(letter ? "The document contains the sequence 'abc'" :
+                "The document does not contain the sequence 'abc'");
+    }
+    public static void checkingNumbers(String documents) {
+        boolean startNumber = documents.startsWith("555");
+        System.out.println(startNumber ? "The document starts with the digits '555'" :
+                "The document does not start with the digits '555'");
+    }
+    public static void checkingNumberEnds(String document) {
+        boolean endsNumber = document.endsWith("1a2b");
+        System.out.println(endsNumber ? "The document ends with these characters '1a2b'" :
+                "The document does not end with these characters '1a2b'");
     }
 }
 
